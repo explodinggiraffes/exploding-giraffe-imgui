85e4471d1ff3c93b532768ad2f22dae8299ddd43
John Koszarek <john.koszarek@gmail.com>
2023-02-08 17:47:23 -0600

    Added atexit handler.

--- ExplodingGiraffes/ExplodingGiraffes.cpp
+++ ExplodingGiraffes/ExplodingGiraffes.cpp
@@ -6,8 +6,16 @@
 /* ------------------------------------------------------------------------------------------------------------------
  * Demonstrates using OpenGL, GLFW, and ImGui together to build an application.
  *-----------------------------------------------------------------------------------------------------------------*/
+
+giraffe::Application application;
+
+void HandleApplicationShutdown() {
+  std::cout << "Shutting down Exploding Giraffes and its subsystems: Good bye\n";
+  application.Shutdown();
+}
+
 int main(int argc, char* argv[]) {
-  giraffe::Application application;
+  std::atexit(HandleApplicationShutdown);
 
   std::cout << "Initializing Exploding Giraffes and its subsystems: We have ignition\n";
   bool success = application.Init();
@@ -18,9 +26,6 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Running the Exploding Giraffes event and rendering loop: Go\n";
   application.Run();
-
-  std::cout << "Shutting down Exploding Giraffes and its subsystems: Good bye\n";
-  application.Shutdown();
 
   return EXIT_SUCCESS;
 }
